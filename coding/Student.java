557019fa4308c9de3e2b2e1f4275e99e8228e36f
tcRanga <tcrranga@gamil.com>
2023-07-22 20:29:44 +0000

    email attribute has been included

--- coding/Student.java
+++ coding/Student.java
@@ -2,7 +2,7 @@ public class Student {
     // Private fields
     private String id;
     private String name;
-
+    private String email;
     // Setter method for 'name'
     public void setName(String name) {
         this.name = name;
@@ -22,13 +22,21 @@ public class Student {
     public String getId() {
         return id;
     }
-
+    public void setEmail(String email){
+        this.email=email;
+    }
+    public String getEmail(){
+        return email;
+    }
+    
     public static void main(String[] args) {
         System.out.println("Hello Pelin");
         Student s = new Student();
         s.setId("1");
         s.setName("Amritya Ranga");
-        System.out.println("ID: " + s.getId() + ", Name: " + s.getName());
+        s.setEmail("RajrangaAmrity@gmail.com");
+        System.out.println("ID: " + s.getId() + ", Name: " + s.getName() + ", Email: " + s.getEmail());
+
     }
 }
 
