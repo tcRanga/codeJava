dedb618ecf4b78c7bec436c4be2707d179afb09c
tcRanga <tcrranga@gamil.com>
2023-07-22 18:12:23 +0000

    Removed one line of code

--- coding/Student.java
+++ coding/Student.java
@@ -2,6 +2,5 @@ public class Student {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("pela");
-        System.out.println("hello minister");
     }
 }
