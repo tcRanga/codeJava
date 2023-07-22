70782c0f04c089e9da4dc780c1c39328c5b5949b
tcRanga <tcrranga@gamil.com>
2023-07-22 18:03:54 +0000

    my first commit

---
+++ coding/Student.java
@@ -0,0 +1,7 @@
+public class Student {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("pela");
+        System.out.println("hello minister");
+    }
+}
